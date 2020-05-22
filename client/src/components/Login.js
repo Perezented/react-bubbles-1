90e0288026a7542557b7b18fed35a7582cab36d3
Michael Perez <mperez6473@gmail.com>
2020-05-22 10:41:40 -0500

    Have the PrivateRoute leading to the BubblePage component.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,7 +25,7 @@ class Login extends React.Component {
             .then((res) => {
                 // res.data.payload
                 localStorage.setItem('token', res.data.payload);
-                this.props.history.push('/dashboard');
+                this.props.history.push('/bubblePage');
             })
             .catch((err) => console.log(err));
     };
