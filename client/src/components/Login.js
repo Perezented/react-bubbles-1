d22496152c29c76e214ae743d2fbc35370a98260
Michael Perez <mperez6473@gmail.com>
2020-05-22 10:25:58 -0500

    Have the useLocalStorage set up and have the login page up.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,61 @@
-import React from "react";
+// make a post request to retrieve a token from the api
+// when you have handled the token, navigate to the BubblePage route
+import React from 'react';
+import { authenticatedAxios } from '../utils/authenticatedAxios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: '',
+        },
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value,
+            },
+        });
+    };
+
+    login = (e) => {
+        e.preventDefault();
+        authenticatedAxios()
+            .post('/api/login', this.state.credentials)
+            .then((res) => {
+                // res.data.payload
+                localStorage.setItem('token', res.data.payload);
+                this.props.history.push('/dashboard');
+            })
+            .catch((err) => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <br />
+                <form onSubmit={this.login}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                        placeholder={'User Name'}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                        placeholder={'Password'}
+                    />
+                    <button>Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
